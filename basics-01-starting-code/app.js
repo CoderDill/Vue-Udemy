c7f83ac1e7bf1f1bb9a3e7c46698ac9abcd00e9d
CrappyDill <mddillon47@gmail.com>
2022-03-16 23:26:24 -0700

    first commit

---
+++ basics-01-starting-code/app.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoal: 'Finish course',
+            vueLink: 'https://vuejs.org'
+        };
+    },
+    methods: {
+        
+    }
+});
+
+app.mount("#user-goal")
