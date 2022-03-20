1f2bcfc5e6b2ff5230207a260275f4cbb6f2ee23
CrappyDill <mddillon47@gmail.com>
2022-03-20 01:24:27 -0700

    Friend Contact List

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    {{ friend.name }}
+  </li>
+  <button>Show Details</button>
+  <ul>
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-cli-02-adding-a-component/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <header>
+          <h2>My Friends</h2>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 654421 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+</style>
