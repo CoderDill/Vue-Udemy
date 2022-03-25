01befaf1f4dd44c08f8066e08b05e108a7d8043f
CrappyDill <mddillon47@gmail.com>
2022-03-24 21:36:13 -0700

    BaseButton

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <TheHeader title="Stored Resources" />
+  <StoredResources :resources="storedResources" />
+</template>
+
+<script>
+import StoredResources from './components/learning-resouces/StoredResources.vue';
+import { ref } from 'vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  name: 'App',
+  setup() {
+    const storedResources = ref([
+      {
+        id: 'official-guide',
+        title: 'Official Guide',
+        description: 'The official guide of Vue.js documentation.',
+        link: 'https://vuejs.org',
+      },
+      {
+        id: 'google',
+        title: 'Google',
+        description: 'Learn to google...',
+        link: 'https://google.com',
+      },
+    ]);
+    return { storedResources };
+  },
+  components: {
+    StoredResources,
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
