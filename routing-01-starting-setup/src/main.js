451c0a2e8f8eccecb1c6114f58b2b056eef5b715
CrappyDill <mddillon47@gmail.com>
2022-04-01 18:46:15 -0700

    redirct and catch all routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -6,15 +6,17 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {path: "/teams", component: TeamsList},
-        { path: "/users", component: UsersList },
-        {path: "/teams/:teamId", component: TeamMembers}
-    ],
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+      { path: '/teams/:teamId', component: TeamMembers, props: true },
+    {path: '/:notFound(.*)', redirect: '/teams'}
+  ],
 });
 const app = createApp(App);
 
-app.use(router)
+app.use(router);
 
 app.mount('#app');
