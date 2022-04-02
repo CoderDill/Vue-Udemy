87a33a277f0765dd0cc5d5efd635bc787f4371d3
CrappyDill <mddillon47@gmail.com>
2022-04-01 19:15:56 -0700

    named routes

--- routing-01-starting-setup/src/components/teams/TeamsItem.vue
+++ routing-01-starting-setup/src/components/teams/TeamsItem.vue
@@ -11,9 +11,10 @@ export default {
   props: ['id', 'name', 'memberCount'],
   computed: {
     teamMemberLink() {
-      return '/teams/' + this.id
-    }
-  }
+      // return '/teams/' + this.id
+      return { name: 'team-members', params: { teamId: this.id } };
+    },
+  },
 };
 </script>
 
