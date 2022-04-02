bd259f4d0057399c84a27efc4722c17bf2029fb2
CrappyDill <mddillon47@gmail.com>
2022-04-01 19:33:27 -0700

    scrollBehavior Vue Router

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -4,20 +4,36 @@ import App from './App.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-      {
-        name: 'teams',
+    {
+      name: 'teams',
       path: '/teams',
-      component: TeamsList,
-      children: [{ name: 'team-members', path: ':teamId', component: TeamMembers, props: true }],
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:notFound(.*)', redirect: '/teams' },
   ],
+  scrollBehavior(to, from, savedPosition) {
+      console.log(to, from, savedPosition);
+      if (savedPosition) {
+          return savedPosition
+      }
+    return { left: 0, top: 0 };
+  },
 });
 const app = createApp(App);
 
