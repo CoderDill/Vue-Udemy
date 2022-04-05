837a63a49edfecf82305e3bdc3400adb8915bd53
CrappyDill <mddillon47@gmail.com>
2022-04-04 18:32:43 -0700

    Use reactive instead of two refs

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -2,7 +2,7 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 v-if="goalVisible">{{ goal }}</h3>
+  <h3 v-if="data.goalVisible">{{ data.goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button @click="goalIsVisible">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -13,13 +13,17 @@
 </template>
 
 <script setup>
-  import {ref} from 'vue'
+  import { reactive} from 'vue'
 
-  const goal = ref('Learn Vue well enough to not get fired 😂')
-  const goalVisible = ref(false)
+  // const goal = ref('Learn Vue well enough to not get fired 😂')
+  // const goalVisible = ref(false)
+  const data = reactive({
+    goal: 'Learn Vue well enough to not get fired 😂',
+    goalVisible: false
+  })
 
   function goalIsVisible() {
-    goalVisible.value = !goalVisible.value;
+    data.goalVisible = !data.goalVisible;
   }
 </script>
 
