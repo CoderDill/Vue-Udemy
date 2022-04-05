aa6f147989e8281fe80a7ded53cf5ed0a9c4996d
CrappyDill <mddillon47@gmail.com>
2022-04-04 18:21:33 -0700

    setup prac

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -2,9 +2,9 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="goalVisible">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="goalIsVisible">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -12,10 +12,15 @@
   <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
-<script>
-export default {
-  
-}
+<script setup>
+  import {ref} from 'vue'
+
+  const goal = ref('Learn Vue well enough to not get fired 😂')
+  const goalVisible = ref(false)
+
+  function goalIsVisible() {
+    goalVisible.value = !goalVisible.value;
+  }
 </script>
 
 <style>
