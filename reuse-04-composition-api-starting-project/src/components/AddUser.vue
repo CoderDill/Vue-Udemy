19fc603edbbef3b43090feef50933b564ab2674f
Matthew Dillon <matt.dillon@launchbadge.com>
2022-04-08 13:25:06 -0700

    Custom Hook

--- reuse-04-composition-api-starting-project/src/components/AddUser.vue
+++ reuse-04-composition-api-starting-project/src/components/AddUser.vue
@@ -8,29 +8,9 @@
   </section>
 </template>
 
-<script>
-import { ref } from 'vue';
-import UserAlert from './UserAlert.vue';
+<script setup>
+import UserAlert from "./UserAlert.vue";
+import useAlert from "../hooks/alert";
 
-export default {
-  components: {
-    UserAlert,
-  },
-  setup() {
-    const alertIsVisible = ref(false);
-
-    function showAlert() {
-      alertIsVisible.value = true;
-    }
-    function hideAlert() {
-      alertIsVisible.value = false;
-    }
-    
-    return {
-      alertIsVisible,
-      showAlert,
-      hideAlert
-    };
-  },
-};
+const { alertIsVisible, showAlert, hideAlert } = useAlert();
 </script>
