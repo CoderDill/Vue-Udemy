2684b316f2250bcd1e507877fc3270c06ad75cbf
CrappyDill <mddillon47@gmail.com>
2022-04-23 18:58:50 -0700

    useCoachStore

--- main-prj-01-starting-setup/src/main.js
+++ main-prj-01-starting-setup/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import router from './router.js'
 import app from './App.vue'
+import { createPinia } from 'pinia'
 
 
-
-createApp(app).use(router).mount('#app');
+createApp(app).use(createPinia()).use(router).mount('#app');
